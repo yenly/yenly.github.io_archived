392730b646e2c21d416497fd74655d1244ce4c14
Yenly Ma <yencodes@gmail.com>
2017-03-04 18:08:37 -0800

    Connect project component to project details component.

--- src/containers/projects.js
+++ src/containers/projects.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import ProjectDetails from './project_details';
+import ProjectDetails from './project_details';
 
 class Projects extends Component {
   renderList() {
     return this.props.projects.map((project) => {
       return (
         <li key={project.name}>
-          <h3>{project.name}</h3>
-          <p>{project.description}</p>
-          <p>{project.tech_stack}</p>
+          <ProjectDetails project={project} />
         </li>
       );
     });
