8bdfab6a5db869d1f5b7f839727aa07467f66b37
Yenly Ma <yencodes@gmail.com>
2017-03-04 18:29:18 -0800

    Make github link to project repo for each project.

--- src/containers/project_details.js
+++ src/containers/project_details.js
@@ -7,6 +7,8 @@ const ProjectDetails = ({project}) => {
       <h3>{project.name}</h3>
       <p>{project.description}</p>
       <p>{project.tech_stack}</p>
+      <a href={project.github_link} className="btn btn-outline-info" target="_blank">Github Repo</a>
+      {/* <a href={project.demo_link} className="btn btn-outline-info" target="_blank">Demo</a> */}
     </div>
   );
 };
